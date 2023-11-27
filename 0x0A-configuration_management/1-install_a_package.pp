5551e309c5c6c131f2a8f277aa8e246450081656
britfitzm <brittanyfitzm99@gmail.com>
2023-11-27 10:46:39 -0800

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install flask version 2.1.0 from pip3
+
+$packages = [ 'python3', 'python3-pip']
+
+# Install python3 and pip3 if not already installed
+package { $packages:
+  ensure => 'installed',
+}
+
+# Install werkzeug downgrade
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Install flask version 2.1.0 with pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
